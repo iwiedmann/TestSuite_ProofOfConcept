01ab864dc42b5b321d749f037113c0abb98ae839
Isaac Wiedmann <iwiedmann@gmail.com>
2017-01-07 00:32:27 -0800

    Stared test test_sub_menu_navigation_bar() with associated helpers.

--- pages/about_us_page.py
+++ pages/about_us_page.py
@@ -2,6 +2,10 @@
 Web page module for the Sunverge "ABOUT US" page.
 """
 
+from copy import deepcopy
+
+from selenium.webdriver.common.by import By
+
 from TestSuite_ProofOfConcept.globals import BASE_URL
 from TestSuite_ProofOfConcept.page import Page
 
@@ -15,11 +19,32 @@ class AboutUsPage(Page):
     _page_title = 'Sunverge Energy | About us'
 
     # locators
-    _sunverge_logo_locator = (By.XPATH, _row_header_xpath + '/div/a/img')
+
+    # collections of locators
+    _all_sections_locators = {
+        "vision_section_locator": (By.ID, 'top-section'),
+        "management_section_locator": (By.ID, 'executive-team'),
+        "directors_section_locator": (By.ID, 'boardsec'),
+        "investors_section_locator": (By.ID, 'investors')
+    }
 
     def go_to_about_us_page(self):
         """
-        Navigate to the about us page directly.
+        Navigate to the about us page
+         directly.
         """
         self.selenium.get(self._about_us_url)
         self.check_page_title()
+
+    def are_all_sections_visible(self):
+        """
+        Checks that all the sections on the page are visible.
+        :return: visibility_results: a dictionary with showing which locators were visible
+        """
+        visibility_results = deepcopy(self._all_sections_locators)
+        for locator_name, locator in self._all_sections_locators.iteritems():
+            if self.is_element_visible(*locator):
+                visibility_results[locator_name] = True
+            else:
+                visibility_results[locator_name] = False
+        return visibility_results
--- tests/test_about_us_page.py
+++ tests/test_about_us_page.py
@@ -2,8 +2,35 @@
 Test for the Sunverge "ABOUT US" page.
 """
 
+import pytest
 
-def test_open_about_us_page(selenium):
-    pass
+from TestSuite_ProofOfConcept.pages.about_us_page import AboutUsPage
 
-# TODO: finish this file
+
+# fixtures
+
+@pytest.fixture
+def setup_about_us_page(selenium):
+    """
+    Setup fixture to directly navigate to the "ABOUT US" page.
+    :return: about_us_pg: object for the "ABOUT US" page
+    """
+    about_us_pg = AboutUsPage(selenium)
+    about_us_pg.go_to_about_us_page()
+    return about_us_pg
+
+
+# tests
+
+def test_sub_menu_navigation_bar(setup_about_us_page):
+    """
+    Parameterized test to test all the links in the green sub menu navigation bar.  Each link is clicked and then it is
+    checked that all the sections are still visible.
+    TODO: A better test might check that the page scrolled to the correct section after clicking each link; however,
+    this test could be fragile.
+    """
+    about_us_pg = setup_about_us_page
+    visibility_results = about_us_pg.are_all_sections_visible()
+    for locator_name, is_visible in visibility_results.iteritems():
+        assert is_visible, "{locator} is not visible".format(locator=locator_name)
+    #TODO: finish this test
